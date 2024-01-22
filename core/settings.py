54da44f8fd26c3c7da2d72cea088eeabf6b9037d
i-mohammad <mg9859094@gmail.com>
2024-01-22 17:36:26 +0330

    Update settings.py
    add rest framework  DEFAULT_AUTHENTICATION_CLASSES

--- core/settings.py
+++ core/settings.py
@@ -131,3 +131,14 @@ MEDIA_URL = '/file/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        # "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.TokenAuthentication",
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ]
+}
+
