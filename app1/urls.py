7a42a4367f903365ebc548e5e83163f59b59bd23
i-mohammad <mg9859094@gmail.com>
2024-01-22 17:41:53 +0330

    Update urls.py

--- app1/urls.py
+++ app1/urls.py
@@ -4,13 +4,13 @@ from .views import *
 app_name = 'account'
 
 urlpatterns = [
-    path('', SignUPView.as_view(), name='signup'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('logout/', LogOutView.as_view(), name='logout'),
-    path('changepassword/', ChangePasswordView.as_view(), name='setpassword'),
-    path('reset/<token>/', ResetPasswordView.as_view(), name='reset_password'),
-
-
+    path('', SignUPAPIView.as_view(), name='signup'),
+    path('register/', RegisterAPIView.as_view(), name='register'),
+    path('logout/', LogOutAPIView.as_view(), name='logout'),
+    path('changepassword/', ChangePasswordAPIView.as_view(), name='setpassword'),
+    path('reset/<token>/', ResetPasswordAPIView.as_view(), name='reset_password'),
+    path('changeprofile/', UpdateProfileAPIView.as_view(), name='change_profile'),
+    path('forgetpassword/', SendLinkForResetPasswordAPIView.as_view(), name='send_reset_password_link'),
 ]
 
 
