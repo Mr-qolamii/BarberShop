4be6189506b23fc51a8b3e43f04c9ba7c0b2a6e3
i-mohammad <mg9859094@gmail.com>
2024-01-22 17:42:26 +0330

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -1,27 +1,22 @@
+from django.contrib.auth import authenticate, login, logout
+from django.utils import timezone
 from rest_framework import status, permissions
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import generics
 from rest_framework.response import Response
-from django.contrib.auth import authenticate, login, logout
 
-from Celery.tasks import *
-from .models import *
+from .tasks import *
 from .serializers import *
 from .permissions import *
+from .models import *
 
 
-class SignUPView(generics.GenericAPIView):
+class SignUPAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
-
     permission_classes = [NotAuthenticated]
 
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'message': 'User Created'}, status=status.HTTP_201_CREATED)
- 
 
-class RegisterView(generics.GenericAPIView):
+class RegisterAPIView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
     permission_classes = [NotAuthenticated]
@@ -30,37 +25,53 @@ class RegisterView(generics.GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = authenticate(self.request, username=serializer.validated_data['username'],
-                                            password=serializer.validated_data['password'])
+                            password=serializer.validated_data['password'])
         if user is not None:
             login(request, user)
+            device = {"user": request.user, "device_name": request.META.get('HTTP_USER_AGENT'),
+                      "login_time": timezone.now(), "is_active": True}
+            Device.objects.create(**device)
             return Response({'detail': 'login success'}, status=status.HTTP_200_OK)
         return Response({'detail': 'Login is not successful'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class LogOutView(generics.GenericAPIView):
+class LogOutAPIView(generics.GenericAPIView):
     """ Logout view """
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
+        Device.objects.get(device_name=request.META.get('HTTP_USER_AGENT'), user_id=request.user.id).delete()
         logout(request)
         return Response({'detail': 'logout success'}, status=status.HTTP_200_OK)
 
 
-class ChangePasswordView(generics.GenericAPIView):
+class ChangePasswordAPIView(generics.GenericAPIView):
     serializer_class = UserSetPasswordSerializer
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        if self.request.user.check_password(serializer.validated_data['password']):
-            self.request.user.set_password(serializer.validated_data['password_now'])
-            login(request, self.request.user)
-            return Response({'detail': 'login success'}, status=status.HTTP_200_OK)
-        raise ValidationError({'detail': 'not login success'})
+        user = User.objects.get(id=request.user.id)
+        if user.check_password(serializer.validated_data['password_old']):
+            user.set_password(serializer.validated_data['password_now'])
+            user.save()
+            return Response({'detail': 'password set success'}, status=status.HTTP_200_OK)
+        raise ValidationError({'detail': 'password is false'})
 
 
-class ResetPasswordView(generics.GenericAPIView):
-    serializer_class = RestPasswordSerializer
+class SendLinkForResetPasswordAPIView(generics.GenericAPIView):
+    serializer_class = SendSMSForResetPasswordSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        send_sms.dlay(tell=serializer.validated_data['tell'], msg=self.request.user.get_session_auth_hash())
+        return Response({'detail': 'The link has been sent to you'})
+
+
+class ResetPasswordAPIView(generics.GenericAPIView):
+    serializer_class = ResetPasswordSerializer
 
     def post(self, request):
         self.request.user.check_password()
@@ -71,3 +82,12 @@ class ResetPasswordView(generics.GenericAPIView):
             return Response({'detail': 'set password success'}, status=status.HTTP_200_OK)
 
         raise ValidationError({'detail': 'password not match'})
+
+
+class UpdateProfileAPIView(generics.UpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        obj = Profile.objects.get(pk=self.request.user.pk)
+        return obj
