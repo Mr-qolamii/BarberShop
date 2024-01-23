36759ecd2fcb845a884eb166ae0a506e27d417f3
i-mohammad <mg9859094@gmail.com>
2024-01-23 10:30:18 +0330

    Update views.py

--- app2/views.py
+++ app2/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import generics, ModelViewSet
+from django_filters.rest_framework import *
+from rest_framework.permissions import *
+from rest_framework.filters import *
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class PostViewSet(ModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = Post.objects.all().order_by('-date')
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ["location", "user", "title"]
+    ordering_fields = ["price"]
+    filterset_fields = {"location": ["exact", "in"], "price": ["lt", "gt"]}
+    serializer_class = PostSerializer
