8cf3e68680bb25db87bd31bdcd189acf3265b60b
i-mohammad <mg9859094@gmail.com>
2024-01-24 16:09:49 +0330

    Update models.py

--- app3/models.py
+++ app3/models.py
@@ -4,27 +4,22 @@ from django.db import models
 
 class Reservation(models.Model):
     """ reservation model """
-    choices = [(1, ""),
-               (2, ""),
-               (3, ""),
-               (4, ""),
-               (5, ""),
-               (6, ""),
-               (7, ""),
-               (8, ""),
-               (9, ""),
-               (10, ""),
-               (11, ""),
-               (12, ""),
-               (13, "")]
+    choices = [(1, "فید"),
+               (2, "هیر کات مدرن"),
+               (3, "پاکسازی"),
+               (4, "پاکسازی و استایل"),
+               (5, "فید و پاکسازی"),
+               (6, "فید و هریکات"),
+               (7, "فید و میکاپ"),
+               (8, "فید و پاکسازی و میکاپ"),
+               (9, "فید و استایل و میکاپ"),
+               (10, "فید و استایل و پاکسازی"),
+               (11, "فید و استایل و میکاپ و پاکسازی"),
+               ]
 
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     date = models.DateTimeField()
-    content = models.CharField(choices=choices)
+    status = models.BooleanField(default=True)
+    content = models.CharField(max_length=30, choices=choices)
 
 
-class CancelledReservation(models.Model):
-    """ cancelled reservation model """
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
-    date = models.DateTimeField()
