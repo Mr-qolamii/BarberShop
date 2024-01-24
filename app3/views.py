87dfa41672fc96a16e938de4c78b09c544689a53
i-mohammad <mg9859094@gmail.com>
2024-01-24 16:10:03 +0330

    Update views.py

--- app3/views.py
+++ app3/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class ReservationViewSet(ModelViewSet):
+    queryset = Reservation.objects.all().order_by('date')
+    serializer_class = ReservationsSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
