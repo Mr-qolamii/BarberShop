2cf9e18fb583f11fcedd207a19b83e4ae1224034
i-mohammad <mg9859094@gmail.com>
2024-01-24 11:45:52 +0330

    Update permissions.py

--- app2/permissions.py
+++ app2/permissions.py
@@ -1,8 +1,14 @@
 from rest_framework.permissions import BasePermission
 
+SAFE_METHODS = ["GET", "HEADER",  "OPTION"]
+
 
 class IsAdminUserOrReadOnly(BasePermission):
-    SAFE_METHODS = ["GET", "HEADER",  "OPTION"]
 
     def has_object_permission(self, request, view, obj):
-        return bool(request.method in self.SAFE_METHODS or request.user and request.user.is_superuser)
+        return bool(request.method in SAFE_METHODS or request.user and request.user.is_superuser)
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(request.method in SAFE_METHODS or request.user == obj.user)
