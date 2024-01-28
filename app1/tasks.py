48461f55ee179649fcb26ab57954cb667472d9f4
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-01-28 22:00:17 +0330

    Update tasks.py

--- app1/tasks.py
+++ app1/tasks.py
@@ -1,20 +1,7 @@
 import os
-from celery import Celery
 
+from core.celery import app
 from .models import *
-
-# Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-app = Celery("app1_tasks", backend='redis://localhost:6379', broker='redis://localhost:6379')
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django apps.
-app.autodiscover_tasks()
 
 
 @app.task
@@ -24,11 +11,12 @@ def send_sms(tell: str, msg: str) -> None:
     reset password link
     {msg}
     """
+    print(message)
 
 
 @app.task
 def create_user(username: str, tell: str, password: str, **extra_field) -> None:
-    return User.objects.create_user(username=username, tell=tell, password=password, **extra_field)
+    User.objects.create_user(username=username, tell=tell, password=password, **extra_field)
 
 
 if __name__ == "__main__":
