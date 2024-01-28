4da910fe0d07963fe5a3f797e6722bc6ea026b6d
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-01-28 22:53:50 +0330

    Update tasks.py

--- app1/tasks.py
+++ app1/tasks.py
@@ -15,7 +15,7 @@ def send_sms(tell: str, msg: str) -> None:
 
 @app.task
 def create_user(username: str, tell: str, password: str, **extra_field) -> None:
-    User.objects.create_user(username=username, tell=tell, password=password, **extra_field)
+    return User.objects.create_user(username=username, tell=tell, password=password, **extra_field)
 
 
 if __name__ == "__main__":
