5e534c53c8047bcde6a858b7afb7b04ca82abc55
i-mohammad <mg9859094@gmail.com>
2024-02-01 14:29:12 +0330

    Update views.py

--- app2/views.py
+++ app2/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import generics, ModelViewSet
+from rest_framework.response import Response
 from django_filters.rest_framework import *
 from rest_framework.permissions import *
 from rest_framework.filters import *
@@ -21,9 +22,31 @@ class PostViewSet(ModelViewSet):
         return super().retrieve(request, *args, **kwargs)
 
 
-class CommentViewSet(ModelViewSet):
-    permission_classes = [IsOwnerOrReadOnly]
-    queryset = Comment.objects.all().order_by('-date')
+class CommentAPIView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(post=self.kwargs['pk'])
+        serializer = self.get_serializer(queryset, menu=True)
+        return Response(data=serializer.data)
 
+
+class PostLike(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = PostLike.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(post=self.kwargs['pk'])
+        if queryset.filter(user=self.kwargs['user']).exists():
+            return Response({"post likes count is": queryset.count(), "is liked": True})
+        else:
+            return Response({"post likes count is": queryset.count(), "is liked": False})
+
+    def create(self, request, *args, **kwargs):
+        kwargs['user'] = request.user
+        kwargs['post'] = kwargs['pk']
+        if not PostLike.objects.filter(post=self.kwargs['pk'], user=kwargs['user']).exists():
+            post_like.apply_async(kwargs=kwargs)
+        return Response({"detail": True})
