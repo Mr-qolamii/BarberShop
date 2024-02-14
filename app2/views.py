d4b075790737e52fea2dba9ebeb6665bc1c7b0d7
i-mohammad <mg9859094@gmail.com>
2024-02-14 14:32:56 +0330

    Update views.py

--- app2/views.py
+++ app2/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.viewsets import generics, ModelViewSet
 from rest_framework.response import Response
 from django_filters.rest_framework import *
@@ -18,7 +19,7 @@ class PostViewSet(ModelViewSet):
 
     def retrieve(self, request, *args, **kwargs):
         if not PostViews.objects.filter(user=request.user, post=self.kwargs['pk']).exists():
-            post_view.apply_async(kwargs={"post": self.kwargs['pk'], "user": self.request.user})
+            post_view.apply_async(kwargs={"post_id": self.kwargs['pk'], "user": self.request.user})
         return super().retrieve(request, *args, **kwargs)
 
 
@@ -36,17 +37,20 @@ class CommentAPIView(generics.ListCreateAPIView):
 class PostLike(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = PostLike.objects.all()
+    serializer_class = PostLikeSerializer
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset().filter(post=self.kwargs['pk'])
-        if queryset.filter(user=self.kwargs['user']).exists():
-            return Response({"post likes count is": queryset.count(), "is liked": True})
+        if queryset.filter(user=request.user).exists():
+            return Response({"post likes count is": queryset.filter(post=self.kwargs['pk']).count(), "is liked": True})
         else:
-            return Response({"post likes count is": queryset.count(), "is liked": False})
+            return Response({"post likes count is": queryset.filter(post=self.kwargs['pk']).count(), "is liked": False})
 
     def create(self, request, *args, **kwargs):
-        kwargs['user'] = request.user
-        kwargs['post'] = kwargs['pk']
-        if not PostLike.objects.filter(post=self.kwargs['pk'], user=kwargs['user']).exists():
-            post_like.apply_async(kwargs=kwargs)
-        return Response({"is liked": True})
+        queryset = self.get_queryset().filter(post=self.kwargs['pk'])
+        if queryset.filter(user=request.user).exists():
+            post_like_delete.apply_async(kwargs={"post_id": kwargs['pk'], "user": request.user})
+            return Response({"is liked ": False}, status=status.HTTP_200_OK)
+        else:
+            post_like.apply_async(kwargs={"post_id": kwargs['pk'], "user": request.user})
+            return Response({"is liked ": True}, status=status.HTTP_200_OK)
