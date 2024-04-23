8d30948d45f7f4290b1d63dc4d89cb79ac97b2be
i-mohammad <mg9859094@gmail.com>
2024-04-23 09:19:32 +0330

    Update permissions.py

--- app1/permissions.py
+++ app1/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import DjangoModelPermissions
 
+from .models import User
 
 class NotAuthenticated(DjangoModelPermissions):
     def has_permission(self, request, view):
@@ -7,3 +8,7 @@ class NotAuthenticated(DjangoModelPermissions):
             return False
         else:
             return True
+
+class ExistToken(DjangoModelPermissions):
+    def has_permission(self, request, view):
+        pass
