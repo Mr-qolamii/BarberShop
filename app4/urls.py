454169730aa0f6f3a70d0ac836df4e1efcac43ea
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-05-08 15:30:34 +0330

    Update urls.py

--- app4/urls.py
+++ app4/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('allorders/', OrderListView.as_view(), name='all_orders'),
     path('soldOut/', SoldOutListView.as_view(), name='sold_out'),
     path('products/<int:pk>/addorder/', CreateOrderView.as_view(), name='add_order'),
+    path('myorder/', GetUserOrderListView.as_view(), name='my_order'),
 ]
 
 urlpatterns += router.urls
