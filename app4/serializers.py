08264ea4213783c7dbdd11eb2513f5ffba00f5bc
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-05-08 17:01:25 +0330

    Update serializers.py

--- app4/serializers.py
+++ app4/serializers.py
@@ -6,14 +6,21 @@ from .models import *
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all_'
+        fields = '__all__'
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['product', 'user', 'count', 'date']
-        extera_kwargs = {'date': {"read_only": True}, }
+        extra_kwargs = {'user': {'read_only': True}, 'product': {'read_only': True}, 'date': {'read_only': True}}
+
+    def validate(self, attrs):
+        attrs['user'] = self.context['request'].user
+        attrs['product'] = Product.objects.get(pk=self.context['request'].parser_context['kwargs']['pk'])
+        if Order.objects.filter(user=attrs['user'], product=attrs['product']).exists():
+            raise serializers.ValidationError("This product is in your shopping cart")
+        return attrs
 
 
 class SoldOutSerializer(serializers.ModelSerializer):
