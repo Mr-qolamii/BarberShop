eab24caa3efd61eb3022c5d1d769da4a1355f580
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-06-17 15:11:02 +0330

    Update urls.py

--- app4/urls.py
+++ app4/urls.py
@@ -7,13 +7,13 @@ from .views import *
 router = routers.SimpleRouter()
 
 router.register('products', ProductViewSet, 'products')
-router.register('orders', OrderModelViewSet, 'orders')
 
 urlpatterns = [
-    path('allorders/', OrderListView.as_view(), name='all_orders'),
+    path('orders/', OrderListView.as_view(), name='all_orders'),
+    path('myorders/<int:pk>/', UpdateOrderView.as_view(), name='order_update'),
     path('soldOut/', SoldOutListView.as_view(), name='sold_out'),
     path('products/<int:pk>/addorder/', CreateOrderView.as_view(), name='add_order'),
-    path('myorder/', GetUserOrderListView.as_view(), name='my_order'),
+    path('myorders/', GetUserOrderListView.as_view(), name='my_order'),
 ]
 
 urlpatterns += router.urls
