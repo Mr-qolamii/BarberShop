0a5e5efa94ced2be77c59f6613b46bca390db092
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-06-17 15:10:52 +0330

    Update serializers.py

--- app4/serializers.py
+++ app4/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import *
+from .tasks import *
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -12,15 +13,20 @@ class ProductSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ['product', 'user', 'count', 'date']
-        extra_kwargs = {'user': {'read_only': True}, 'product': {'read_only': True}, 'date': {'read_only': True}}
+        fields = ['pk', 'product', 'user', 'count', 'date', 'is_done']
+        extra_kwargs = {'pk': {'read_only': True}, 'user': {'read_only': True}, 'product': {'read_only': True},
+                        'date': {'read_only': True}, 'is_done': {'read_only': True}}
 
     def validate(self, attrs):
         attrs['user'] = self.context['request'].user
-        attrs['product'] = Product.objects.get(pk=self.context['request'].parser_context['kwargs']['pk'])
-        if Order.objects.filter(user=attrs['user'], product=attrs['product']).exists():
+        attrs['product_id'] = self.context['request'].parser_context['kwargs']['pk']
+        if Order.objects.filter(user=attrs['user'], product=attrs['product']).exists() and self.context[
+            'request'].method == 'POST':
             raise serializers.ValidationError("This product is in your shopping cart")
         return attrs
+
+    def create(self, validated_data):
+        return create_order.apply_async(kwargs=validated_data).get()
 
 
 class SoldOutSerializer(serializers.ModelSerializer):
