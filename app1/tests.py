15887e742787fde8ba71cf64a11a65891edfaf22
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-06-17 15:09:29 +0330

    Update tests.py

--- app1/tests.py
+++ app1/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+import random
 
 from app1.models import *
 
@@ -9,20 +10,18 @@ class App1TestCase(TestCase):
         self.user = User.objects.create_user(username='tset', tell='+989904761632', password='test@1234')
 
     def test_register_response(self):
-        response = self.client.login(username='tset', password='test@1234')
+        response = self.client.post('http://127.0.0.1:8000/register/',
+                                    data={"username": 'tset', "password": 'test@1234'},
+                                    headers={'Content-Type': 'application/json', 'Accept': 'application/json',
+                                             "HTTP_USER_AGENT": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"})
         self.assertTrue(response)
 
     def test_logout_response(self):
-        response = self.client.login(username='tset', password='test@1234')
+        self.client.login(username='tset', password='test@1234')
+        response = self.client.post('http://127.0.0.1:8000/register/',
+                                    data={"username": 'tset', "password": 'test@1234'},
+                                    headers={'Content-Type': 'application/json', 'Accept': 'application/json',
+                                             "HTTP_USER_AGENT": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"})
         response = self.client.get('http://127.0.0.1:8000/logout/', ).status_code
         self.assertIs(response, 200)
 
-    def test_signup_response(self):
-        response = self.client.post('http://127.0.0.1:8000/',
-                                    data={'username': 'tset2',
-                                          'tell': '+989904761638',
-                                          'password': 'test2@1234',
-                                          'password_2': 'test2@1234'
-                                          }
-                                    ).status_code
-        self.assertIs(response, 201)
