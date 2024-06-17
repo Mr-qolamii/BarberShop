9ec27fe466107f6b1029f64373d57b24b5a63dc1
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-06-17 15:10:48 +0330

    Update models.py

--- app4/models.py
+++ app4/models.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.utils.datetime_safe import datetime
 from django.dispatch.dispatcher import receiver
 from django.contrib.auth import get_user_model
@@ -8,17 +8,17 @@ User = get_user_model()
 
 
 class Product(models.Model):
-    file1 = models.FileField(upload_to='products',blank=True)
-    file2 = models.FileField(upload_to='products',blank=True)
-    file3 = models.FileField(upload_to='products',blank=True)
-    file4 = models.FileField(upload_to='products',blank=True)
-    file5 = models.FileField(upload_to='products',blank=True)
-    file6 = models.FileField(upload_to='products',blank=True)
+    file1 = models.FileField(upload_to='products', blank=True)
+    file2 = models.FileField(upload_to='products', blank=True)
+    file3 = models.FileField(upload_to='products', blank=True)
+    file4 = models.FileField(upload_to='products', blank=True)
+    file5 = models.FileField(upload_to='products', blank=True)
+    file6 = models.FileField(upload_to='products', blank=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    category = models.CharField(max_length=50, choices=[(1, 'machine'), (2, 'kif vasayel'), (3, 'arayeshi behdashti')])
+    category = models.CharField(max_length=50, choices=[(1, 'machine'), (2, 'kif vasayel'), (3, 'arayeshi_behdashti')])
     price = models.IntegerField()
-    SoldOut = models.BooleanField(default=False)
+    count = models.IntegerField(default=1)
 
     def __str__(self):
         return self.title
@@ -29,9 +29,8 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     count = models.IntegerField(default=1)
+    is_done = models.BooleanField(default=False)
 
 
-class SoldOut(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+
+
