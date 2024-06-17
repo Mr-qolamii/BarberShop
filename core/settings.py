72ffc5973faf3326e71bc2c6a671d1be8c6e5793
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-06-17 15:11:14 +0330

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'source/'
-STATIC_ROOT = BASE_DIR / 'static/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+STATIC_ROOT = BASE_DIR/'static/'
+MEDIA_ROOT = BASE_DIR/'media/'
 MEDIA_URL = 'file/'
 
 # Default primary key field type
