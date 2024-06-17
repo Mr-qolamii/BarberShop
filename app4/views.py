0d40bba4ea12dec1945d0eb0913661a7642c4dc9
i-mohammad <mohammadhoseyngholamy@gmail.com>
2024-06-17 15:11:11 +0330

    Update views.py

--- app4/views.py
+++ app4/views.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import generics
@@ -15,8 +16,9 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (IsAdminUserOrReadOnly,)
-    filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ('category',)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset_fields = ('category', )
+    search_fields = ('description',)
 
 
 class OrderListView(generics.ListAPIView):
@@ -25,19 +27,8 @@ class OrderListView(generics.ListAPIView):
     permission_classes = (IsAdmin,)
 
 
-class OrderModelViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    permission_classes = (IsAdmin,)
-    filter_backends = (DjangoFilterBackend,)
-
-    def get_object(self):
-        obj = self.queryset.filter(user=self.request.user)
-        return obj
-
-
 class SoldOutListView(generics.ListAPIView):
-    queryset = Order.objects.all().order_by('-id')
+    queryset = Order.objects.filter(is_done=True).order_by('-id')
     serializer_class = SoldOutSerializer
     permission_classes = (IsAdmin,)
     filter_backends = (DjangoFilterBackend,)
@@ -45,21 +36,39 @@ class SoldOutListView(generics.ListAPIView):
 
 
 class CreateOrderView(generics.CreateAPIView):
-        serializer_class = OrderSerializer
-        permission_classes = (IsAuthenticated,)
-        queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Order.objects.all()
 
-        def create(self, request, *args, **kwargs):
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class GetUserOrderListView(generics.ListAPIView):
     serializer_class = OrderSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = Order.objects.filter(user=self.request.user).order_by('-id')
+        return queryset
+
+
+class UpdateOrderView(generics.DestroyAPIView, generics.RetrieveUpdateAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = (IsOwner,)
+
+    def get_queryset(self):
+        queryset = Order.objects.filter(user=self.request.user).order_by('-id')
+        return queryset
+
+
+class RetrieveOrderView(generics.RetrieveAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = (IsOwner,)
 
     def get_queryset(self):
         queryset = Order.objects.filter(user=self.request.user).order_by('-id')
